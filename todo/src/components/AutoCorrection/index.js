b1ace5872ddd41d09a48383a06c32c04230b641f
natraj-vemula <70875652+natraj-vemula@users.noreply.github.com>
2024-02-15 17:56:45 +0530

    Add arrow btn functionality

--- todo/src/components/AutoCorrection/index.js
+++ todo/src/components/AutoCorrection/index.js
@@ -7,7 +7,7 @@ const AutoCorrection = () => {
     // State declarations
     const [word, setWord] = useState("");
     const [corrections, setCorrections] = useState([]);
-
+    const [selectedItem, setSelectedItem] = useState(-1)
     // Event Handlers
     const onChange = async (event) => {
         const inputWord = event.target.value;
@@ -28,13 +28,24 @@ const AutoCorrection = () => {
         setWord(word)
         setCorrections([])
     }
+
+    const handleKeyDown = e => {
+        if (e.key === "ArrowUp" && selectedItem > 0){
+            setSelectedItem(prev => prev - 1)
+        }else if (e.key === "ArrowDown" && selectedItem < corrections.length - 1){
+            setSelectedItem(prev => prev + 1)
+        }else if (e.key === "Enter" && selectedItem >= 0){
+            setWord(corrections[selectedItem])
+            setCorrections([])
+        }
+    }
     return (
         <div className="suggestion-box">
             <h1 className="heading">Auto Correction Suggestor</h1>
             <div className="box-inner">
-                <input type="text" placeholder="check" value={word} onChange={e => onChange(e)} className="suggestion-input" />
+                <input type="text" placeholder="check" value={word} onChange={e => onChange(e)} className="suggestion-input" onKeyDown={e => handleKeyDown(e)}/>
                 {word && (<div className="dropdown"><ul >
-                    {corrections.map((v, i) => <li key={i} onClick={() => onSearch(v)} >{v}</li>)}
+                    {corrections.map((v, i) => <li key={i} onClick={() => onSearch(v)} className={selectedItem === i ? "active":""} >{v}</li>)}
                 </ul></div>)}
             </div>
         </div>
